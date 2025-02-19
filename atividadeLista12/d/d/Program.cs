3103ba7218da4eef1d14910bc572d11eef5f2751
Robertinhou <robertmenezesp9@gmail.com>
2025-02-19 09:02:31 -0300

    Comitão da lista 12A

--- atividadeLista12/d/d/Program.cs
+++ atividadeLista12/d/d/Program.cs
@@ -2,38 +2,41 @@
  * Created by SharpDevelop.
  * User: Aluno_Manha
  * Date: 19/02/2025
- * Time: 07:59
+ * Time: 08:12
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+
 using System.Collections.Generic;
-namespace c
+
+namespace d
 {
 	class Program
 	{
 		public static void Main(string[] args)
 		{
 			
-			List<int> nums = new List<int>{9, 2, 8, 10, 5};
 			
-			int maior = 0;
 			
-			foreach (int i in nums) {
+			List<string> palavras = new List<string>{"Abcd", "aaaa", "zé", "daMangaaaa"};
+			
+			int quantLetras = 0;
+			string maiorPalavra = "";
+			
+			foreach(string palavra in palavras){
 				
-				if(i > maior){
-					
-					maior = i;
-					
+				if(palavra.Length > quantLetras){
+					quantLetras = palavra.Length;
+					maiorPalavra = palavra;
 				}
 				
 			}
 			
-			Console.WriteLine("O maior é: "+ maior);
+			Console.WriteLine("A maior palavra é {0} com {1} letras", maiorPalavra, quantLetras);
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
-			
 		}
 	}
 }
--- atividadeLista12/e/e/Program.cs
+++ atividadeLista12/e/e/Program.cs
@@ -2,38 +2,35 @@
  * Created by SharpDevelop.
  * User: Aluno_Manha
  * Date: 19/02/2025
- * Time: 07:59
+ * Time: 08:35
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+
 using System.Collections.Generic;
-namespace c
+
+namespace e
 {
 	class Program
 	{
 		public static void Main(string[] args)
 		{
 			
-			List<int> nums = new List<int>{9, 2, 8, 10, 5};
-			
-			int maior = 0;
+			List<int> nums = new List<int>{ 1, 2, 3 ,4 ,5 ,6 };
+			 
+			int soma = 0;
 			
 			foreach (int i in nums) {
 				
-				if(i > maior){
-					
-					maior = i;
-					
-				}
+				soma += i;
 				
 			}
 			
-			Console.WriteLine("O maior é: "+ maior);
+			Console.WriteLine("A média é: "+ (double) soma / nums.Count );
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
-			
 		}
 	}
 }
